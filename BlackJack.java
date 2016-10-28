ce8dbf9ddb687f1a53865ebe28bc6ba93d54b8da
Mariusz Krz <krzyzowski.mariusz@gmail.com>
2016-10-28 22:15:34 +0200

    add croupier's movements

--- BlackJack.java
+++ BlackJack.java
@@ -38,7 +38,13 @@ class BlackJack {
         
      //game
         prepare();
+        int gamer_sum = gamer();
+        int croupier_sum = croupier();
+      //200  sum();
+        
     }
+   /*This function recovers default settings and 2 cards are dealt to croupier and gamer
+    */
     public void prepare()
     { //add cards to deck and remove (if it isn't first round) cards from decks of croupier and gamer
         for(int i=0; i<4; i++)
@@ -52,23 +58,59 @@ class BlackJack {
       //add two cards into decks of croupier and gamer
         int index;
         for(int i=0;i<2;i++)
-        {
-            index = rand.nextInt(deckOfCards.size());
+        {   index = rand.nextInt(deckOfCards.size());
             gamerCards.add(deckOfCards.get(index));
             deckOfCards.remove(index);
         }
         
         for(int i=0;i<2;i++)
-        {
-            index = rand.nextInt(deckOfCards.size());
+        {   index = rand.nextInt(deckOfCards.size());
             croupierCards.add(deckOfCards.get(index));
             deckOfCards.remove(index);
         }
-        for(int i=0;i<deckOfCards.size();i++) System.out.print(" ( "+i+")"+deckOfCards.get(i)+"  ");
-        System.out.println("\nKarty gracza");
-        for(int i=0;i<gamerCards.size();i++) System.out.print("  "+gamerCards.get(i)+"  ");
-        System.out.println("\nKarty krupiera");
-        for(int i=0;i<croupierCards.size();i++) System.out.print("  "+croupierCards.get(i)+"  ");
+     //displays information about both cards of gamer and one of the cards of croupier   
+        System.out.printf("Karty gracza: %10d %10d\n", gamerCards.get(0), gamerCards.get(1));
+        System.out.printf("Karta krupiera: %10d", croupierCards.get(0));
+        
+    
+       
+    }
+    /* This function decides about changes in croupier's cards using the rules:
+    1. if sum of his cards is <=16 -> crouper hits new card
+    2. if sum is >16 and there's no ace in cards -> crouper stands
+    3. if sum is >21 and there's ace in cards -> ace changes value to 1'
+    Default value of ace is 11.
+    */
+    public int gamer()
+    {
+        
+    }
+    public int croupier() //function returns sum of croupier's cards after potential automatic changes
+    { int part_sum;
+      boolean var = false;
+      SimulatorCasino.cls();
+      
+        do{part_sum=0;
+           System.out.println("\n\n\nKrupier ma nastepujace karty:");
+           for(int x: croupierCards) System.out.print("  "+x+"  ");
+                System.out.println("\n");
+           for(int x : croupierCards)
+            part_sum+=x;
+            System.out.println("Suma kart krupiera to: "+part_sum);
+           if(part_sum>16) 
+               if (part_sum>21&&croupierCards.contains(11)) {croupierCards.set(croupierCards.lastIndexOf(11), 1);
+                               System.out.println("W kartach byl as, a suma kart przekroczyla 21, wiec w miejsce asa wstawiamy wartosc 1");}
+               else var = true; 
+           else {hit(croupierCards); System.out.println("Krupier dobiera nowa karte...");}
+        } while(var == false);
+        
+        return part_sum;
     }
     
+    public void hit(List<Integer> name)
+    {
+            int index = rand.nextInt(deckOfCards.size());
+            name.add(deckOfCards.get(index));
+            deckOfCards.remove(index);
+    }
 }
